193fab083473bbb74f067c1bb4f4a3434e64a3c8
Андрей Лавринюк <splaandrey@gmail.com>
2017-04-04 23:04:21 +0300

    Объекты h:12:02

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var user = {};
+user.name = "John";
+user.age = 25;
+
+var petro = {name:"Petro", age:33};
+ 
+//  print(typeof(user));
+//  print_tag(typeof(user),"h1");
+print(user);
+print_tag(user.name,"h1");
+print(petro);
+print_tag(petro.name,"h1");
+print_tag(petro.age,"h1");
+petro.admin = true;
---
+++ js/splaa-function.js
@@ -0,0 +1,9 @@
+function print(str){
+ document.writeln(str + "<br />")
+}
+function print_tag(str, tag){
+ document.writeln("<"+ tag +">" + str + "</"+ tag +"><br />");
+}
+function hr(){
+    document.writeln("<hr>hello");
+}
